9c86cb49b6e966282c5680e6f9f8bd4edf6ca352
Patrick Scott <patrick.lee.scott@gmail.com>
2014-04-16 03:56:54 -0400

    idle timeout

--- client/main.js
+++ client/main.js
@@ -9,21 +9,25 @@ Global client-side code. Loads last.
 //
 
 Meteor.startup(function () {
+
+  // Add FastClick
   FastClick.attach(document.body);
+
+
+  // Allow touch scrolling on .touch-scrollable elements
   document.body.addEventListener('touchmove', function(event) {
     if (! $(event.target).parents().hasClass("touch-scrollable" ))
     {
       event.preventDefault();
     }
   }, false);
-});
 
-Meteor.startup(function() {
+
   // Initialize Mixpanel Analytics
   mixpanel.init('37f6902be1f2618c7cf2a5b37dbef276'); //YOUR TOKEN
-});
 
-Meteor.startup(function() {
+
+  // Subscribe to device data when a device ID is available
   Deps.autorun(function () {
     var deviceId = Session.get('deviceId'),
       device = Devices.findOne(deviceId);
@@ -32,4 +36,5 @@ Meteor.startup(function() {
       Meteor.subscribe('deviceData');
     }
   });
+
 });
