2a0fcb3b3b1ac65f033352320b6eedff154534db
Patrick Scott <patrick.lee.scott@gmail.com>
2014-04-17 14:44:01 -0400

    move confirm logic to admin

--- collections/orders.js
+++ collections/orders.js
@@ -118,6 +118,7 @@ Meteor.methods({
       requestedAt: new Date(),
       read: false,
       open: true,
+      status: 'pending',
       userId: user._id
     }
 
@@ -126,7 +127,7 @@ Meteor.methods({
     this.unblock();
 
     if (Meteor.isServer) {
-      var url = stripTrailingSlash(Meteor.settings.apps.device.url) + Router.routes["patronOrderPage"].path({_id: orderId});
+      var url = stripTrailingSlash(Meteor.settings.apps.admin.url) + "/patron-order/{0}".format(orderId);
       var date = moment(reservation.date);
       var formattedDate = date.format("dddd, MMM Do YYYY");
 
@@ -153,42 +154,5 @@ Meteor.methods({
     }
 
     return orderId;
-  },
-  confirmPatronReservation: function(orderId) {
-    var order = Orders.findOne(orderId);
-    if (!order) {
-      throw new Meteor.Error(403, 'Not a valid order'); 
-    }
-
-    Orders.update(orderId, {$set: {open: false, confirmationDate: new Date()}});
-
-    this.unblock();
-
-    if (Meteor.server) {
-      var experience = Experiences.findOne(order.reservation.experienceId);
-      var reservation = order.reservation;
-      var date = moment(reservation.date);
-      var formattedDate = date.format("dddd, MMM Do YYYY");
-
-      Email.send({
-        to: reservation.emailAddress,
-        from: 'order-service@plusmoretablets.com',
-        subject: 'You reservation for {0} has been confirmed.'.format(experience.title),
-        text: "Your reservation for {0} has been confirmed.\n\n".format(experience.title)
-            + "Reservation Details:\n"
-            + "\tFor: {0}\n".format(experience.title)
-            + "\tWhen: {0} at {1}:{2} {3}\n".format(formattedDate, reservation.timeHour, reservation.timeMinute, reservation.timePeriod)
-            + "\tParty Name: {0}\n".format(reservation.partyName)
-            + "\tParty Size: {0}\n".format(reservation.partySize)
-            + "\tPhone #: {0}\n".format(reservation.phoneNumber)
-            + "\tEmail: {0}\n".format(reservation.emailAddress)
-            + "\nVenue Info"
-            + "\n\t{0}".format(experience.venueName)
-            + "\n\t{0}".format(experience.street)
-            + "\n\t{0}, {1} {2}".format(experience.city, experience.state, experience.zip)
-            + "\n\t{0}".format(experience.phone)
-            + "\n\nIf you have any questions, you may respond directly to this email."
-      });
-    }
   }
 });
