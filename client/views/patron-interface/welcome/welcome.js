84315ea1c28eebee7a4674ed7eb5ce1a0d6784cf
Patrick Scott <patrick.lee.scott@gmail.com>
2014-04-22 18:23:52 -0400

    quick welcome screen, fixed up orders

--- client/views/patron-interface/welcome/welcome.js
+++ client/views/patron-interface/welcome/welcome.js
@@ -4,3 +4,10 @@ Template.welcome.helpers({
     return Devices.findOne(deviceId);
   }
 });
+
+Template.welcome.events({
+	'click .welcome-container': function (e) {
+		e.preventDefault();
+		Router.go('orders');
+	}
+});
