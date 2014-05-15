6fc52305fd6c538c1f885da43dfb9ca108daa31c
Patrick Scott <patrick.lee.scott@gmail.com>
2014-05-15 19:16:57 -0400

    only allow gestures when not in reservation process

--- client/lib/gestures.js
+++ client/lib/gestures.js
@@ -1,31 +1,45 @@
 Meteor.startup(function() {
   var hammer = $(document.body).hammer();
-  hammer.on('swipeleft', '.experience', function() {
-    var currentExperienceId = Router.current().params._id;
-    var currentIndex = ExperiencesForCategory.indexOf(currentExperienceId);
-    if (currentIndex + 1 === ExperiencesForCategory.length) {
-      Router.go('experience', {_id: ExperiencesForCategory[0]});
-    } else {
-      Router.go('experience', {_id: ExperiencesForCategory[currentIndex + 1]});
+  hammer.on('swipe', '.experience', function(e) {
+    debugger;
+    var experienceState = Deps.nonreactive(function() {
+      return Session.get('experienceState');
+    });
+
+    if (!experienceState) {
+      switch (e.gesture.direction) {
+        case 'up':
+          nextAnimation = 'up';
+          Router.go('experiences', {category: Session.get('activeCategory')});
+          break;
+        case 'down': 
+          nextAnimation = 'down';
+          Router.go('experiences', {category: Session.get('activeCategory')});
+          break;
+        case 'right':
+          nextAnimation = 'back';
+          var currentExperienceId = Router.current().params._id;
+          var currentIndex = ExperiencesForCategory.indexOf(currentExperienceId);
+          if (currentIndex  === 0) {
+            nextAnimation = 'back';
+            Router.go('experience', {_id: ExperiencesForCategory[ExperiencesForCategory.length - 1]});
+          } else {
+            nextAnimation = 'back';
+            Router.go('experience', {_id: ExperiencesForCategory[currentIndex - 1]});
+          }
+          break;
+        case 'left':
+          nextAnimation = 'default'
+          var currentExperienceId = Router.current().params._id;
+          var currentIndex = ExperiencesForCategory.indexOf(currentExperienceId);
+          if (currentIndex + 1 === ExperiencesForCategory.length) {
+            Router.go('experience', {_id: ExperiencesForCategory[0]});
+          } else {
+            Router.go('experience', {_id: ExperiencesForCategory[currentIndex + 1]});
+          }
+          break;
+      }
     }
-  });
-  hammer.on('swiperight', '.experience', function() {
-    var currentExperienceId = Router.current().params._id;
-    var currentIndex = ExperiencesForCategory.indexOf(currentExperienceId);
-    if (currentIndex  === 0) {
-      nextAnimation = 'back';
-      Router.go('experience', {_id: ExperiencesForCategory[ExperiencesForCategory.length - 1]});
-    } else {
-      nextAnimation = 'back';
-      Router.go('experience', {_id: ExperiencesForCategory[currentIndex - 1]});
-    }
-  });
-  hammer.on('swipedown', '.experience', function() {
-    Router.go('experiences', {category: Session.get('activeCategory')});
-  });
-  hammer.on('swipeup', '.experience', function() {
-    nextAnimation = 'up';
-    Router.go('experiences', {category: Session.get('activeCategory')});
   });
 
   hammer.on('swipe', '.welcome', function(e) {
