8661e4b3d89454273912b9bf3c927e8b40605a78
Patrick Scott <patrick.lee.scott@gmail.com>
2014-10-03 18:05:40 -0400

    filter group options

--- client/views/experiences/experiencesSubnav.js
+++ client/views/experiences/experiencesSubnav.js
@@ -1,5 +1,37 @@
 Template.experiencesSubnav.helpers({
-  tagsForFilterGroup: function () {
-    return ['Test1', 'Test2'];
+  categoryFilterGroupTags: function() {
+    var results = [];
+    if (this.category && this.category.filterGroupTags && this.category.filterGroupTags.length > 0) {
+      var experiencesForCategory = Experiences.find({categoryId: this.category._id});
+
+      _.each(this.category.filterGroupTags, function(group) {
+        
+        var groupOptions = [];
+        experiencesForCategory.forEach(function(experience) {
+          var tagGroupKey = group+"Tags";
+          if (experience[tagGroupKey] && experience[tagGroupKey].length > 0) {
+            _.each(experience[tagGroupKey], function (groupOption) {
+              var option = _.findWhere(groupOptions, {name: groupOption});
+              if (option) {
+                option.count = option.count + 1;
+              } else {
+                groupOptions.push({
+                  name: groupOption,
+                  count: 1
+                });
+              }
+            });
+          }
+          
+        });
+
+        results.push({
+          group: group,
+          options : groupOptions
+        });
+      });  
+    }
+
+    return results;
   }
 });
