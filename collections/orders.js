da6acf3ba9177b84602dda645b61431c4d8fcacb
Patrick Scott <patrick.lee.scott@gmail.com>
2014-10-14 16:24:42 -0400

    fix cancel request

--- collections/orders.js
+++ collections/orders.js
@@ -327,5 +327,13 @@ Meteor.methods({
     }
 
     return orderId;
-  }
+  }, 
+  cancelRequest: function(orderId) {
+    var order = Orders.findOne(orderId);
+    if (!order) {
+      throw new Meteor.Error(403, 'Not a valid order'); 
+    }
+
+    Orders.update(orderId, {$set: {open: false, status: 'cancelled', cancelledDate: new Date()}});
+  },
 });
