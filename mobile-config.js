9117bd5517c40b0107df9b2456d99844af3cda46
Patrick Scott <patrick.lee.scott@gmail.com>
2014-10-24 20:08:15 -0400

    icons and splashscreens organized

--- mobile-config.js
+++ mobile-config.js
@@ -3,52 +3,60 @@
 App.info({
   id: 'com.plusmoretablets.device',
   name: 'PlusMore',
-  description: 'Get über power in one button click',
+  description: 'Hotel automation and digital concierge.',
   author: 'PlusMore Team',
   email: 'info@plusmoretablets.com',
   website: 'http://plusmoretablets.com'
 });
 
 App.icons({
-  // iOS
-  'iphone': 'resources/icons/icon-57px.png',
-  'iphone_2x': 'resources/icons/icon-57px@2x.png',
-  'ipad': 'resources/icons/icon-72px.png',
-  'ipad_2x': 'resources/icons/icon-72px@2x.png',
+  'iphone': 'resources/icons/ios/app/icon-60px.png',
+  'iphone_2x': 'resources/icons/ios/app/icon-60px@2x.png',
+  'iphone_3x': 'resources/icons/ios/app/icon-60px@3x.png',
 
-  // Android - XXX these are the same as iOS for now
-  'android_ldpi': 'resources/icons/icon-36px.png',
-  'android_mdpi': 'resources/icons/icon-48px.png',
-  'android_hdpi': 'resources/icons/icon-72px.png',
-  'android_xhdpi': 'resources/icons/icon-48px@2x.png'
+  'ipad': 'resources/icons/ios/app/icon-72px.png',
+  'ipad_2x': 'resources/icons/ios/app/icon-72px@2x.png',
+
+  'android_ldpi': 'resources/icons/android/app/icon-36px.png',
+  'android_mdpi': 'resources/icons/android/app/icon-48px.png',
+  'android_hdpi': 'resources/icons/android/app/icon-72px.png',
+  'android_xhdpi': 'resources/icons/android/app/icon-96px.png'
 });
 
 App.launchScreens({
   // iOS
-  'iphone': 'resources/splash/ios/320x480.png',
-  'iphone_2x': 'resources/splash/ios/320x480@2x.png',
-  'iphone5': 'resources/splash/ios/640x1136.png',
+  'iphone': 'resources/splash/ios/Default.png', // 320x480
+  'iphone_2x': 'resources/splash/ios/Default@2x.png', // 640x960
 
-  'iphone6': 'resources/splash/ios/750x1334.png',
-  'iphone6p_portrait': 'resources/splash/ios/1242x2208.png',
-  'iphone6p_landscape': 'resources/splash/ios/2208x1242.png',
+  'iphone5': 'resources/splash/ios/Default-568h@2x.png', // 640x1136
 
-  'ipad_portrait': 'resources/splash/ios/768x1024.png',
-  'ipad_portrait_2x': 'resources/splash/ios/768x1024@2x.png',
+  'iphone6': 'resources/splash/ios/Default~iphone6.png', // 750x1334
 
-  'ipad_landscape': 'resources/splash/ios/1024x768.png',
-  'ipad_landscape_2x': 'resources/splash/ios/1024x768@2x.png',
+  'iphone6p_portrait': 'resources/splash/ios/Default~iphone6p.png', // 1242x2208
+  'iphone6p_landscape': 'resources/splash/ios/Default-Landscape~iphone6p.png', // 2208x1242
+
+  'ipad_portrait': 'resources/splash/ios/Default~ipad.png', // 768x1004
+  'ipad_portrait_2x': 'resources/splash/ios/Default@2x~ipad.png', // 1536x2008
+
+  'ipad_landscape': 'resources/splash/ios/Default-Landscape~ipad.png', // 1024x748
+  'ipad_landscape_2x': 'resources/splash/ios/Default-Landscape@2x~ipad.png', // 2048x1496
 
   // Android
-  'android_mdpi_portrait': 'resources/splash/android/mdpi/960x640.9.png',
-  'android_mdpi_landscape': 'resources/splash/android/mdpi/960x640.9.png',
-  'android_hdpi_portrait': 'resources/splash/android/hdpi/960x640.9.png',
-  'android_hdpi_landscape': 'resources/splash/android/hdpi/960x640.9.png',
-  'android_xhdpi_portrait': 'resources/splash/android/xhdpi/960x640.9.png',
-  'android_xhdpi_landscape': 'resources/splash/android/xhdpi/960x640.9.png'
+  'android_ldpi_portrait': 'resources/splash/android/200x320.png',
+  'android_ldpi_landscape': 'resources/splash/android/320x200.png',
+
+  'android_mdpi_portrait': 'resources/splash/android/320x480.png',
+  'android_mdpi_landscape': 'resources/splash/android/480x320.png',
+
+  'android_hdpi_portrait': 'resources/splash/android/480x800.png',
+  'android_hdpi_landscape': 'resources/splash/android/800x480.png',
+
+  'android_xhdpi_portrait': 'resources/splash/android/720x1280.png',
+  'android_xhdpi_landscape': 'resources/splash/android/1280x720.png'
 });
 
 App.setPreference('StatusBarOverlaysWebView', 'false');
 App.setPreference('StatusBarBackgroundColor', '#000000');
+App.setPreference('StatusBarStyle', 'blackopaque');
 
 
