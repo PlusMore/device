c50482170c0e767f78ae3f0ec1746d2c6e8dfe83
Patrick Scott <patrick.lee.scott@gmail.com>
2014-11-04 19:34:54 -0500

    experiences publications

--- client/views/experience/experience.js
+++ client/views/experience/experience.js
@@ -32,3 +32,15 @@ var events = {};
 events[clickevent + " a.back"] = handleBack;
 
 Template.experience.events(events);
+
+Meteor.startup(function() {
+  Tracker.autorun(function() {
+    var currentExperienceId = Session.get('currentExperienceId');
+
+    if (currentExperienceId) {
+      subscriptions.experience = Meteor.subscribe('experience', currentExperienceId);
+    } else {
+      subscriptions.experience.stop();
+    }
+  });
+})
