04a1ff1a165b98c649307b70cfb7f60202eb0fb0
Patrick Scott <patrick.lee.scott@gmail.com>
2014-11-19 16:44:36 -0500

    get form info

--- client/views/select-user/selectUser.js
+++ client/views/select-user/selectUser.js
@@ -43,20 +43,45 @@ var hideModal = function() {
 
 Template.selectUser.events({
   'click [data-dismiss="modal"]':function(e, tmpl){
-    $(document).trigger('cancel-user-selected');
+    console.log('dismiss');
+
     hideModal();
+    $(document).trigger('cancel-user-selected');
     
+  
   }, 
   'click #btn-step-one': function(e, tmpl) {
-    // debugger;
+    // validate
+    var firstName = tmpl.$('[name=firstName]').val();
+    var lastName = tmpl.$('[name=lastName]').val();
+    var checkoutDate = tmpl.$('[name=checkoutDate]').val();
+
+    Session.set('onboardAccountCreationOptions', {
+      profile: {
+        firstName: firstName,
+        lastName: lastName
+      },
+      checkoutDate: checkoutDate
+    });
+
     tmpl.$(tmpl.firstNode).trigger('onboard-step-one-complete');
     
   },
   'onboard-step-one-complete': function(e, tmpl) {
+    // create
     Session.set('onboardStep', 2);
   },
   'click #btn-step-two': function(e, tmpl) {
-    // debugger;
+    //validate
+    var email = tmpl.$('[name=email]').val();
+
+    var accountOptions = Session.get('onboardAccountCreationOptions');
+
+    accountOptions = _.extend(accountOptions, {
+      email: email
+    });
+    Session.set('onboardAccountCreationOptions', accountOptions);
+
     tmpl.$(tmpl.firstNode).trigger('onboard-step-two-complete');
     
   },
@@ -64,15 +89,32 @@ Template.selectUser.events({
     Session.set('onboardStep', 3);
   },
   'click #btn-step-three': function(e, tmpl) {
-    // debugger;
+    // validate
+    var password = tmpl.$('[name=password]').val();
+
+    var accountOptions = Session.get('onboardAccountCreationOptions');
+
+    accountOptions = _.extend(accountOptions, {
+      password: password
+    });
+    Session.set('onboardAccountCreationOptions', accountOptions);
+
     tmpl.$(tmpl.firstNode).trigger('onboard-step-three-complete');
     
   },
   'onboard-step-three-complete': function(e, tmpl) {
-    Session.set('onboardStep', 'complete');
-    Meteor.setTimeout(function() {
-      tmpl.$(tmpl.firstNode).trigger('onboard-complete');
-    }, 2000);
+    var accountOptions = Session.get('onboardAccountCreationOptions');
+    var checkoutDate = accountOptions.checkoutDate;
+    accountOptions = _.pick(accountOptions, ['profile', 'email', 'password'])
+
+    Accounts.createUser(accountOptions, function() {
+      Meteor.call('registerStay', checkoutDate, function() {
+        Session.set('onboardStep', 'complete');
+        Meteor.setTimeout(function() {
+          tmpl.$(tmpl.firstNode).trigger('onboard-complete');
+        }, 2000);
+      });
+    });
   },
   'onboard-complete': function(e, tmpl) {
     $(document).trigger('user-selected');
