a1242943cfa98eafb2d4d4f5c1c8ff0aa75f9372
bfturner <brendanfturner@gmail.com>
2015-01-11 19:45:36 -0600

    final cleanup/tweaks

--- client/views/hotel-information/hotelInformation.js
+++ client/views/hotel-information/hotelInformation.js
@@ -2,9 +2,6 @@ Template.hotelInformation.helpers({
 	hotel: function () {
 		return Hotels.findOne();
 	},
-	inRoom: function () {
-		return LocalStore.get('inRoom');
-	},
 	amenities: function () {
 		return HotelAmenities.find();
 	}
