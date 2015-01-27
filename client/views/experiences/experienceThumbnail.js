6d4026aef96696f341cc46208b2d72324e706e8a
Patrick Scott <patrick.lee.scott@gmail.com>
2015-01-27 03:31:16 -0500

    remove comment

--- client/views/experiences/experienceThumbnail.js
+++ client/views/experiences/experienceThumbnail.js
@@ -1,10 +1,21 @@
 Template.experienceThumbnail.events({
   'click .experienceThumbnail': function (e, tmpl) {
-    console.log('click experienceThumbnail');
     // don't open when user just meant to open perspective container
     if (!$(e.currentTarget).closest('.perspective').hasClass('modalview')) { 
       Session.set('currentExperienceId', this._id);
       experienceModal.show('experience');
     }
   }
+});
+
+Meteor.startup(function() {
+  Tracker.autorun(function() {
+    debugger;
+    var expId = Session.get('currentExperienceId');
+    if (!!expId && !experienceModal.open()) {
+      Meteor.setTimeout(function() {
+        experienceModal.show('experience');
+      },0)
+    }
+  });  
 });
