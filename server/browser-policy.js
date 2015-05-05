139d4f86f27589fd90b16bfe35a54ed41a4015c1
Patrick Scott <patrick.lee.scott@gmail.com>
2015-05-05 11:40:56 -0400

    only only secure where possible

--- server/browser-policy.js
+++ server/browser-policy.js
@@ -1,24 +1,44 @@
 Meteor.startup(function() {
-  // CDN
-  BrowserPolicy.content.allowOriginForAll("https://*.filepicker.io");
-  BrowserPolicy.content.allowOriginForAll("http://*.filepicker.io");
-  BrowserPolicy.content.allowOriginForAll("http://*.mxpnl.com");
-  // Fonts & Icons
-  BrowserPolicy.content.allowOriginForAll("https://*.gstatic.com");
-  BrowserPolicy.content.allowOriginForAll("http://*.gstatic.com");
-  BrowserPolicy.content.allowOriginForAll("https://*.googleapis.com");
-  BrowserPolicy.content.allowOriginForAll("http://*.googleapis.com");
-  BrowserPolicy.content.allowOriginForAll("https://*.google.com");
-  BrowserPolicy.content.allowOriginForAll("http://*.google.com");
-  BrowserPolicy.content.allowOriginForAll("https://fontastic.s3.amazonaws.com");
-  BrowserPolicy.content.allowOriginForAll("http://fontastic.s3.amazonaws.com");
-  BrowserPolicy.content.allowOriginForAll("https://*.cloudfront.net");
-  // PlusMore Apps
-  BrowserPolicy.content.allowOriginForAll("http://*.plusmoretablets.com");
-  BrowserPolicy.content.allowOriginForAll("https://*.plusmoretablets.com");
-  // misc
-  BrowserPolicy.content.allowOriginForAll("https://*.yelp.com");
-  BrowserPolicy.content.allowOriginForAll("http://*.yelp.com");
-  BrowserPolicy.content.allowOriginForAll("https://*.kadira.io");
-  BrowserPolicy.content.allowOriginForAll("http://*.kadira.io");
+
+  BrowserPolicy.content.disallowInlineScripts();
+  // TODO: Damn it, Google.
+  BrowserPolicy.content.allowEval();
+
+  var trustedSecure = [
+    'fontastic.s3.amazonaws.com',
+    '*.filepicker.io',
+    '*.googleapis.com',
+    '*.gstatic.com',
+    '*.cloudfront.net',
+    '*.plusmoretablets.com',
+    '*.plusmore.io',
+    '*.yelp.com',
+    '*.kadira.io'
+  ];
+
+  var trustedBoth = [
+    '*.mxpnl.com',
+    '*.yelpcdn.com'
+  ];
+
+  _.each(trustedSecure, function(trustedDomain) {
+    allowDomainAsOrigin(trustedDomain, false)
+  });
+
+  _.each(trustedBoth, function(trustedDomain) {
+    allowDomainAsOrigin(trustedDomain, true)
+  });
+
 });
+
+var allowDomainAsOrigin = function(domain, allowHttp) {
+  if (allowHttp) {
+    origin = "http://" + domain; // this should only be allowed when over http, don't know how to do that though
+    BrowserPolicy.content.allowOriginForAll(origin);
+  }
+
+  secureOrigin = "https://" + domain;
+  BrowserPolicy.content.allowOriginForAll(secureOrigin);
+}
+
+
