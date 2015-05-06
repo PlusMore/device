9cd4ad5a7a608d6446b17d464851369ef544086e
Patrick Scott <patrick.lee.scott@gmail.com>
2015-05-06 18:31:19 -0400

    unblock sending of email

--- src/aggregates/Accounts/methods/handleNoPasswordSet.js
+++ src/aggregates/Accounts/methods/handleNoPasswordSet.js
@@ -1,5 +1,6 @@
 Meteor.methods({
   handleNoPasswordSet: function(userId) {
+    this.unblock(); // don't need to wait for the server to send the email
     if (Meteor.isServer) {
       return Accounts.sendResetPasswordEmail(userId);
     }
