5fe9c15311dc0cb35bf798c86ea6e37759316a36
Patrick Scott <patrick.lee.scott@gmail.com>
2015-05-25 22:34:01 -0400

    hide filters on mobile because of bug in ios

--- client/views/experiences/experiences.js
+++ client/views/experiences/experiences.js
@@ -1,11 +1,16 @@
 Template.experiences.helpers({
   experiencesLayout: function() {
+    if (ResponsiveHelpers.isXs()) {
+      // these are broken on mobile ios, so just don't show for now
+      return 'contentLayout';
+    }
+
     if (this.category) {
       if (this.category.filterGroupTags && this.category.filterGroupTags.length > 0) {
-        return "subnavContentLayout";
+        return 'subnavContentLayout';
       }
     }
-    return "contentLayout";
+    return 'contentLayout';
   },
   hasSubnav: function() {
     if (this.category) {
